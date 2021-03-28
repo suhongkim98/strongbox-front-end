ef5f508f04da1b70f96291798129b186e10aafa7
suhongkim98 <kshz9815@naver.com>
2021-03-28 18:24:55 +0900

    연락하기 페이지 구현

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -3,11 +3,14 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
 width: 100%;
+padding: 0 20px 0 20px;
 ${({theme}) => theme.media.desktop`  
     width: 1000px;
+    padding: 0 0 0 0;
 `}
 ${({theme}) => theme.media.tabletM`  
     width: 700px;
+    padding: 0 0 0 0;
 `}
 `;
 
--- src/router/Contact.tsx
+++ src/router/Contact.tsx
@@ -1,7 +1,88 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Container from '../components/Container';
+import Footer from '../components/Footer';
+import Span from '../components/Span';
+import theme from '../styles/theme';
+const TotalWrapper = styled.div`
+    display: grid;
+    grid-template-rows: 60px 1fr 200px;
+    ${({theme}) => theme.media.tabletM`  
+    grid-template-rows: 60px 1fr 200px;
+    `}
+    ${({theme}) => theme.media.tabletS`  
+    grid-template-rows: 60px 1fr 100px;
+    `}
+    ${({theme}) => theme.media.mobile`  
+    grid-template-rows: 60px 1fr 100px;
+    `}
+`;
+const HeaderWrapper = styled.header`
+    background-color: ${theme.colors.mainColor};
+    display: flex;
+    align-items: center;
+    padding: 0 0 0 30px;
+`;
+const BodyWrapper = styled.section`
+    display: grid;
+    grid-template-rows: 300px 1fr;
+`;
+const BodyHeader = styled.header`
+    border-bottom: 1px solid gray;
+    display: flex;
+    justify-content: center;
+`;
+const BodyHeaderInner = styled.div`
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+const BodyInner = styled.div`
+    display: flex;
+    justify-content: center;
+`;
 
+const TextItem = styled.div`
+    display: flex;
+    border-left: 6px solid black;
+    padding: 0 30px 0 30px;
+`;
+const BodyMain = styled.div`
+    height: 100vh;
+    padding: 20px 0 0 0;
+`;
 const Contact = () => {
-    return (<div>a</div>);
+    return (<TotalWrapper>
+        <HeaderWrapper><Link to="/"><Span textColor="white" fontWeight="700" size="1.6rem">Accong Box</Span></Link></HeaderWrapper>
+        <BodyWrapper>
+            <BodyHeader>
+                <Container>
+                    <BodyHeaderInner>
+                        <TextItem>
+                            <div>
+                                <Span fontWeight="700" size="3rem">Accong Box</Span><br/><br/><br/>
+                                <Span size="2rem">건의하기</Span>
+                            </div>
+                        </TextItem>
+                        <div>
+                            이미지
+                        </div>
+                    </BodyHeaderInner>
+                </Container>
+            </BodyHeader>
+            <BodyInner>
+                <Container>
+                    <BodyMain>
+                        <Span fontWeight="700" size="2rem">개발자 연락처<br /><br /></Span>
+                        <Span fontWeight="700" size="1.4rem">Email: </Span><Span size="1.4rem">suhong.kim@kakao.com</Span>
+                    </BodyMain>
+                </Container>
+            </BodyInner>
+        </BodyWrapper>
+        <Footer/>
+    </TotalWrapper>);
 }
 
 export default Contact;
